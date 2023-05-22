305a5435409a7f2d56351ca82376dc1cb81a37bb
Dustan Kasten <dustan.kasten@gmail.com>
2023-05-22 18:11:28 -0400

    lol node v4. Update to v18

--- install.sh
+++ install.sh
@@ -90,16 +90,12 @@ install_fnm() {
 }
 
 install_node() {
-  # TODO install these independently
   if ! cmd_exists "node"
   then
-    nvm install v4.4.4 &>/dev/null
-    nvm install v7 &>/dev/null
-    print_success "node v4.4.4 installed"
-    print_success "node v7.x.x installed"
+    fnm install v18 &>/dev/null
+    print_success "node v18.x.x installed"
   else
-    print_success "node v4.4.4 already installed"
-    print_success "node v7.x.x already installed"
+    print_success "node v18.x.x already installed"
   fi
 
   if ! cmd_exists "yarn"
