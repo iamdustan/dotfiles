a465345902e966f621c55825ca44905486d5edd3
Dustan Kasten <dustan.kasten@gmail.com>
2025-12-23 08:39:33 -0500

    lazydev.nvim for neovim config

--- config/nvim/lua/plugins/completion/init.lua
+++ config/nvim/lua/plugins/completion/init.lua
@@ -1,5 +1,16 @@
 return {
 	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
